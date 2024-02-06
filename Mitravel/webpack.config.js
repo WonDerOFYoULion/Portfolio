ea9badb294a94fc232e8dcbcdf1ebf91f27b2e07
Zinkin_A/A <artemij.zinkovich@mail.ru>
2024-02-06 13:39:33 +0300

    build

--- Mitravel/webpack.config.js
+++ Mitravel/webpack.config.js
@@ -37,19 +37,7 @@ const optimization = () => {
                 {
                   plugins: [
                     {
-                      name: "preset-default",
-                      params: {
-                        overrides: {
-                          removeViewBox: false,
-                          addAttributesToSVGElement: {
-                            params: {
-                              attributes: [
-                                { xmlns: "http://www.w3.org/2000/svg" },
-                              ],
-                            },
-                          },
-                        },
-                      },
+                      removeViewBox: false,
                     },
                   ],
                 },
